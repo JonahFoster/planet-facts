0ba7672a631969f1dc756f857531da52697fbf2d
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-02 15:24:33 -0400

    Initial Planet component commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './Header.jsx'
-
+import Planet from './Planet.jsx'
 
 export default function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <Planet />
+    </div>
     // for item in data.json, planet Component, feed component prop of object, inside the component reference the prop object
   )
 }
---
+++ src/Planet.jsx
@@ -0,0 +1,5 @@
+export default function Planet() {
+    return (
+        <p>Mercury</p>
+    )
+}
