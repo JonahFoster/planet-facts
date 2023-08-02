fb4e9e2dbb8b20713b0699d8a3d6b9c72de97c8a
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-02 14:28:40 -0400

    Initial Header implementation

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+
+  return (
+    <header>
+        <p className="title">The Planets</p>
+        <nav>
+            <ul>
+                <li>Mercury</li>
+                <li>Venus</li>
+                <li>Earth</li>
+                <li>Mars</li>
+                <li>Jupiter</li>
+                <li>Saturn</li>
+                <li>Uranus</li>
+                <li>Neptune</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
