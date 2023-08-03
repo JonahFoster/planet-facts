74ff53562493c13ce0d6a7c9846afefe31a930be
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-03 12:34:47 -0400

    Basic planet component styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,5 @@ export default function App() {
       <Header onPlanetClick={handlePlanetClick} />
       <Planet planet={selectedPlanet} />
     </div>
-    // for item in data.json, planet Component, feed component prop of object, inside the component reference the prop object
-    // Need state for planet, selections on nav bar will trigger function to change the state
-    // new planet selection becomes fed to planet component
-    // add event listener to each nav list item
   )
 }
--- src/Planet.jsx
+++ src/Planet.jsx
@@ -4,25 +4,47 @@ export default function Planet(props) {
   const planetData = data.find(planet => planet.name === props.planet)
 
   return (
-    <div>
-        <h1>{props.planet}</h1>
-        <p>{planetData.overview.content}</p>
-        <p>Source :<a href={planetData.overview.source}> Wikipedia</a></p>
-        <p>01</p>
-        <h2>Overview</h2>
-        <p>02</p>
-        <h2>Internal Structure</h2>
-        <p>03</p>
-        <h2>Surface Geology</h2>
-        <img src={planetData.images.planet} />
-        <p>Rotation Time</p>
-        <p>{planetData.rotation}</p>
-        <p>Revolution Time</p>
-        <p>{planetData.revolution}</p>
-        <p>Radius</p>
-        <p>{planetData.radius}</p>
-        <p>Average Temp.</p>
-        <p>{planetData.temperature}</p>
-    </div>
+    <section>
+      <div className='planet-main-container'>
+        <div className='planet-img-container'>
+          <img src={planetData.images.planet} />
+        </div>
+        <div className='planet-info-container'>
+          <h1>{props.planet}</h1>
+          <p className='planet-content'>{planetData.overview.content}</p>
+          <p className ='source'>Source : <a href={planetData.overview.source} className="wikipedia">Wikipedia</a></p>
+          <div className='planet-topic-container'>
+            <p className='planet-topic-number'>01</p>
+            <p className='planet-topic'>Overview</p>
+          </div>
+          <div className='planet-topic-container'>
+            <p className='planet-topic-number'>02</p>
+            <p className='planet-topic'>Internal Structure</p>
+          </div>
+          <div className='planet-topic-container'>
+            <p className='planet-topic-number'>03</p>
+            <p className='planet-topic'>Surface Geology</p>
+          </div>
+        </div>
+      </div>
+      <div className='planet-facts-container'>
+        <div className='planet-fact-box'>
+          <p className='planet-fact-title'>Rotation Time</p>
+          <p className='planet-fact'>{planetData.rotation}</p>
+        </div>
+        <div className='planet-fact-box'>
+          <p className='planet-fact-title'>Revolution Time</p>
+          <p className='planet-fact'>{planetData.revolution}</p>
+        </div>
+        <div className='planet-fact-box'>
+          <p className='planet-fact-title'>Radius</p>
+          <p className='planet-fact'>{planetData.radius}</p>
+        </div>
+        <div className='planet-fact-box'>
+          <p className='planet-fact-title'>Average Temp.</p>
+          <p className='planet-fact'>{planetData.temperature}</p>
+        </div>
+        </div>
+    </section>
   );
 }
